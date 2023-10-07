1b9561872780eb563e8aaa1a4ac29eb017feae66
Gatsinzimeek <hackergastinzi@gmail.com>
2023-10-08 00:13:34 +0300

    updated now

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,3 +1,13 @@
--- If it exists, drop it; otherwise, do nothing
-USE hbtn_0c_0;
-SHOW TABLES;
+#!/bin/bash
+
+-- Check if a database name is provided as an argument
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <database_name>"
+    exit 1
+fi
+
+-- Get the database name from the command-line argument
+database_name="$1"
+
+-- MySQL command to list tables in the specified database
+mysql -u your_username -p -e "USE $database_name; SHOW TABLES;"
--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,6 +1,16 @@
--- Script for creation of first_table in hbtn_0c_0 DB
-    USE hbtn_0c_0;
-    CREATE TABLE IF NOT EXISTS first_table(id INT,
-                        name VARCHAR(256) 
-                    );
-    
+-- Check if a database name is provided as an argument
+IF [ $# -eq 0 ]; THEN
+    echo "Usage: $0 <database_name>"
+    exit 1
+fi
+
+-- Get the database name from the command-line argument
+database_name="$1"
+
+-- MySQL command to create the 'first_table' if it doesn't exist
+mysql -u your_username -p "$database_name" -e "
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
+"
